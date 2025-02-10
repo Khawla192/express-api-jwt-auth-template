fd1e28ac021474c3b2334bddd387ff549cd414d6
Khawla192 <202005171@stu.uob.edu.bh>
2025-02-10 19:29:52 +0300

    use a token payload to protect a route from unauthorized users

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+require('./config/database');
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const logger = require('morgan');
+
+// Import the controller file
+const testJwtRouter = require('./controllers/test-jwt');
+const authRouter = require('./controllers/auth');
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+// PUBLIC ROUTES
+
+// PRIVATE ROUTES
+app.use('/auth', authRouter);
+app.use('/test-jwt', testJwtRouter);
+app.use('/users.js', )
+// LISTENER
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
